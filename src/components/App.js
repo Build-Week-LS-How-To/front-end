3c42434daf9f93e1e9bd61d2185592457ac996c7
Chris Lien <clien@sharpencx.com>
2020-08-20 17:16:38 -0400

    added sass variables, styling and navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 
+// STYLES
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/global.scss'
+
 // COMPONENTS
 import Navigation from './Navigation/Navigation'
 import HomePage from './Home/Home'
 import Login from './Login/Login'
 import CreateAccount from './CreateAccount/CreateAccount'
-
-// STYLES
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import {
+  Container
+} from 'reactstrap'
 
 function App() {
   return (
     <div className="App">
         <Navigation />
-        <Switch>
-          <Route exact path="/" component={ HomePage } />
-          <Route exact path="/login" component={ Login } />
-          <Route exact path="/create-account" component={ CreateAccount } />
-        </Switch>
+        <Container>
+          <Switch>
+            <Route exact path="/" component={ HomePage } />
+            <Route exact path="/login" component={ Login } />
+            <Route exact path="/create-account" component={ CreateAccount } />
+          </Switch>
+        </Container>
     </div>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import { Navbar, Nav, NavItem } from 'reactstrap';
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Container
+} from 'reactstrap';
+
 import { Link } from 'react-router-dom'
+
+// STYLES
+import '../../styles/navigation.scss'
 
 const Navigation = () => {
 
-    return(
-        <Navbar>
-            <Nav>
-                <NavItem>
-                    <Link to='/'>Home</Link>
-                </NavItem>
-                
-                <NavItem>
-                    <Link to='/login'>Login</Link>
-                </NavItem>
+    const [isOpen, setIsOpen] = useState(false);
 
-                <NavItem>
-                    <Link to='/create-account'>Create Account</Link>
-                </NavItem>
+    const toggle = () => setIsOpen(!isOpen);
+  
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+            <Container>
+            <div className="navigation-inner">
 
-            </Nav>
+                <NavbarBrand>
+                    <h1><Link to='/'>How To</Link></h1>
+                </NavbarBrand>
+
+                <div className="nav-right">
+                    <NavbarToggler onClick={toggle} />
+                    <Collapse isOpen={isOpen} navbar>
+                        <Nav className="mr-auto" navbar>
+                            <NavItem>
+                                <NavLink>
+                                    <Link to='/'>About</Link>
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink>
+                                    <Link to='/'>Meet The Team</Link>
+                                </NavLink>
+                            </NavItem>
+
+                            <UncontrolledDropdown nav inNavbar >
+                                <DropdownToggle nav caret className="nav-dropdown">
+                                    <span>Log In</span>
+                                </DropdownToggle>
+                                <DropdownMenu right>
+                                    <Link to='/login'><DropdownItem>Sign In</DropdownItem></Link>
+                                    <DropdownItem divider />
+                                    <Link to='/create-account'><DropdownItem>Create Account</DropdownItem></Link>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                    </Collapse>
+                </div>
+            </div>
+            </Container>
         </Navbar>
+      </div>
     )
 }
 
