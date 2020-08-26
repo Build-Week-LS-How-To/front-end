6e1b1752ce4c1025b16c24b8a25db887b7047ae1
Mac <xuromeo@gmail.com>
2020-08-25 17:56:35 -0700

    searchbar

---
+++ src/components/Home/SearchBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+//Styles
+import '../../styles/searchbar.scss';
+//Components
+import {
+    Form,
+    Button,
+    Card,
+    Input
+} from 'reactstrap';
+
+
+export default function SearchBar() {
+    return (
+        <Card>
+            <Form className='searchbar-form'>
+                <Input placeholder='Search for Life Hacks' type='text' />
+                <Button>Search</Button>
+            </Form>
+        </Card>
+    )
+}
+
